666bff79c7ead148707893d80446292acc1d65ab
Tundelhn <tunde.lhn@gmail.com>
2019-12-20 17:48:42 -0500

    get single

---
+++ src/Components/tracks/Tracks.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Consumer } from '../../Context';
+import Spinner from '../layouts/Spinner';
+import Track from './Track';
+const Tracks = () => {
+  return (
+    <Consumer>
+      {value => {
+        const { track_list, heading } = value;
+        if (track_list === undefined || track_list.length === 0) {
+          return <Spinner />;
+        } else {
+          return (
+            <React.Fragment>
+              <h3 className="text-center mb-4">{heading}</h3>
+              <div className="row">
+                {track_list.map(item => (
+                  <Track key={item.track.track_id} track={item.track} />
+                ))}
+              </div>
+            </React.Fragment>
+          );
+        }
+      }}
+    </Consumer>
+  );
+};
+export default Tracks;
